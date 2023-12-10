00095d1b1bfda9f2bbe69362e5e7e88966b1d82d
DaemonLife <52444457+DaemonLife@users.noreply.github.com>
2023-12-10 23:21:10 +0300

    Add files via upload

---
+++ Counting images in scribus.py
@@ -0,0 +1,16 @@
+import scribus
+from scribus import BUTTON_OK
+
+page = 1
+pagenum = scribus.pageCount()
+images_content = 0
+while (page <= pagenum):
+    scribus.gotoPage(page)
+    d = scribus.getPageItems()
+    for item in d:
+        if item[1] == 2:
+            images_content += 1
+    page += 1
+#print("Images in file:")
+print(images_content)
+scribus.messageBox('Book Spine Width', 'Images: ' + str(images_content), BUTTON_OK)
